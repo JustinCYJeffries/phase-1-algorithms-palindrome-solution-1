819c2edc18169d0fcc9a5662afae1e20987ca30b
Justin Jeffries <justincyjeffries@gmail.com>
2022-02-24 22:49:17 -0700

    commit tree bb898382bfe2999a136fd171e17b62d49c6847e2

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let wordArray = word.split('')
+  let reverso = wordArray.reverse()
+  let together = reverso.join('')
+  return together === word
 }
 
 /* 
-  Add your pseudocode here
+  reverse the input
+
+  if input = the reverse 
+    return true
+  else return false
 */
 
 /*
-  Add written explanation of your solution here
+  split the word into an array, with each letter as an element
+  reversed the order of elements using .reverse
+  join the reversed together back into a string
+  check if the string matches the input
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +31,30 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
 }
+
 
 module.exports = isPalindrome;
